6d7d7dffec82748ce5a9c0d5556432562025fb80
meiramura <220764@astanait.edu.kz>
2024-02-12 04:44:14 +0600

    new commit

--- app.js
+++ app.js
@@ -259,6 +259,6 @@ async function getTimeZone(lat, lon) {
     }
 }
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
